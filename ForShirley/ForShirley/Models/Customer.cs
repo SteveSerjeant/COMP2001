2bfea34cb4c8c3af5f76147daed223e78c0d5b0a
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2020-11-19 13:19:32 +0000

    first commit

---
+++ ForShirley/ForShirley/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForShirley.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Cust_Name { get; set }
+        public string Cust_Addr { get; set; }
+        public string Cust_Postcode { get; set; }
+        public string Cust_Phone { get; set; }
+
+    }
+}
